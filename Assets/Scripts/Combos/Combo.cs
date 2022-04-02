8bdef82fce6225f83302877e5b9d7fdbeac18e23
thieflin <ignaciogarrido1234@hotmail.com>
2022-04-02 14:08:31 -0300

    Combos funcionando

--- Assets/Scripts/Combos/Combo.cs
+++ Assets/Scripts/Combos/Combo.cs
@@ -4,66 +4,121 @@ using UnityEngine;
 
 public class Combo : MonoBehaviour
 {
-    public int comboCounter;
-    public float comboUptime;
-    public float comboTime;
-    public bool isComboing;
-    public bool comboEnded;
-    public Animator anim;
-    public bool canAttack =true;
-    public float comboDowntime, comboDowntimeFixedValue;
+    bool _isIdle = true;
+    public Animator ani;
+    int _nextCombo = 0;
 
-
-
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0) && canAttack)
+        InputController();
+    }
+
+    void InputController()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            if (comboCounter == 0)
+            if (_isIdle == true)
             {
-               // anim.SetBool("Att1", true);
-                anim.SetTrigger("A1");
-                Debug.Log("Ataque numero 1 del combo");
-                comboCounter++;
-                isComboing = true;
+                _isIdle = false;
+                ani.SetTrigger("A1");
             }
-            else if (comboCounter == 1 && !Input.GetKey(KeyCode.LeftShift))
-            {
-                Debug.Log("Ataque numero 2 del combo");
-                comboCounter++;
-                anim.SetTrigger("A2");
-                isComboing = true;
-            }
-            else if (comboCounter == 2)
-            {
-                comboCounter++;
-                anim.SetTrigger("A3");
-                Debug.Log("Ataque numero 3 del combo rapido");
-                Debug.Log("Finisher with melee");
-                comboEnded = true;
-            }
+            else
+                _nextCombo = 1;
         }
-
-        if (isComboing)
+        else if (Input.GetKeyDown(KeyCode.Mouse1))
         {
-            comboTime -= Time.deltaTime;
+            _nextCombo = 2;
         }
+    }
 
-        if (comboTime <= 0)
+    public void EventNextAnimation()
+    {
+        switch (_nextCombo)
         {
-            comboCounter = 0;
-            isComboing = false;
-            comboTime = comboUptime;
+            case 0:
+                ani.SetTrigger("Idle");
+                _isIdle = true;
+                break;
+            case 1:
+                ani.SetTrigger("A1");
+                _nextCombo = 0;
+                break;
+            case 2:
+                ani.SetTrigger("A2");
+                _nextCombo = 0;
+                break;
         }
+    }
 
-        if (comboEnded)
-        {
-            isComboing = false;
-            comboCounter = 0;
-            comboEnded = false;
-            comboTime = comboUptime;
-        }
-
+    public void FinishCombo()
+    {
+        _nextCombo = 0;
+        ani.SetTrigger("Idle");
+        _isIdle = true;
     }
 }
+    #region my combo
+    //public int comboCounter;
+    //public float comboUptime;
+    //public float comboTime;
+    //public bool isComboing;
+    //public bool comboEnded;
+    //public Animator anim;
+    //public bool canAttack =true;
+    //public float comboDowntime, comboDowntimeFixedValue;
+
+
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Mouse0) && canAttack)
+    //    {
+    //        if (comboCounter == 0)
+    //        {
+    //           // anim.SetBool("Att1", true);
+    //            anim.SetTrigger("A1");
+    //            Debug.Log("Ataque numero 1 del combo");
+    //            comboCounter++;
+    //            isComboing = true;
+    //        }
+    //        else if (comboCounter == 1 && !Input.GetKey(KeyCode.LeftShift))
+    //        {
+    //            Debug.Log("Ataque numero 2 del combo");
+    //            comboCounter++;
+    //            anim.SetTrigger("A2");
+    //            isComboing = true;
+    //        }
+    //        else if (comboCounter == 2)
+    //        {
+    //            comboCounter++;
+    //            anim.SetTrigger("A3");
+    //            Debug.Log("Ataque numero 3 del combo rapido");
+    //            Debug.Log("Finisher with melee");
+    //            comboEnded = true;
+    //        }
+    //    }
+
+    //    if (isComboing)
+    //    {
+    //        comboTime -= Time.deltaTime;
+    //    }
+
+    //    if (comboTime <= 0)
+    //    {
+    //        comboCounter = 0;
+    //        isComboing = false;
+    //        comboTime = comboUptime;
+    //    }
+
+    //    if (comboEnded)
+    //    {
+    //        isComboing = false;
+    //        comboCounter = 0;
+    //        comboEnded = false;
+    //        comboTime = comboUptime;
+    //    }
+
+    //}
+    #endregion
+
