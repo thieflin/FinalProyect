36778cdd2cb29fce1f89bdb5290c23c955dd3d56
thieflin <ignaciogarrido1234@hotmail.com>
2022-04-02 02:51:10 -0300

    a

--- Assets/Scripts/Combos/Combo.cs
+++ Assets/Scripts/Combos/Combo.cs
@@ -18,53 +18,30 @@ public class Combo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.Mouse0) && canAttack)
         {
             if (comboCounter == 0)
             {
+               // anim.SetBool("Att1", true);
                 anim.SetTrigger("A1");
                 Debug.Log("Ataque numero 1 del combo");
                 comboCounter++;
                 isComboing = true;
             }
-            //else if (comboCounter == 1 && !Input.GetKey(KeyCode.LeftShift))
-            //{
-            //    Debug.Log("Ataque numero 2 del combo");
-            //    comboCounter++;
-            //    anim.SetTrigger("A2");
-            //    isComboing = true;
-            //}
-            ////else if (comboCounter == 1 && Input.GetKey(KeyCode.LeftShift))
-            ////{
-            ////    comboCounter++;
-            ////    Debug.Log("Ataque numero 2 finisher pesado pesado");
-            ////    Debug.Log("Finisher with melee");
-            ////    comboEnded = true;
-            ////}
-            //else if (comboCounter == 2)
-            //{
-            //    comboCounter++;
-            //    anim.SetTrigger("A3");
-            //    Debug.Log("Ataque numero 3 del combo rapido");
-            //    Debug.Log("Finisher with melee");
-            //}
-        }
-
-        if (Input.GetKeyDown(KeyCode.Mouse1))
-        {
-            if (comboCounter == 2 && !Input.GetKey(KeyCode.LeftShift))
+            else if (comboCounter == 1 && !Input.GetKey(KeyCode.LeftShift))
             {
+                Debug.Log("Ataque numero 2 del combo");
                 comboCounter++;
-                Debug.Log("Ataque numero 3 del combo");
-                Debug.Log("Finisher with shotgun regular");
-                comboEnded = true;
-            }
-            else if (comboCounter == 2 && Input.GetKey(KeyCode.LeftShift))
-            {
-                Debug.Log("Ataque numero 3 del combo");
-                comboCounter++;
-                comboCounter++; Debug.Log("Heavy Finisher");
+                anim.SetTrigger("A2");
                 isComboing = true;
+            }
+            else if (comboCounter == 2)
+            {
+                comboCounter++;
+                anim.SetTrigger("A3");
+                Debug.Log("Ataque numero 3 del combo rapido");
+                Debug.Log("Finisher with melee");
+                comboEnded = true;
             }
         }
 
