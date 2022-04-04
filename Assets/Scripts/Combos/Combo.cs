eef68f0899f8119c3af552b15efd5994261ef119
thieflin <ignaciogarrido1234@hotmail.com>
2022-04-04 02:54:55 -0300

    Agregue cosas del combo para que se vea mejor

--- Assets/Scripts/Combos/Combo.cs
+++ Assets/Scripts/Combos/Combo.cs
@@ -7,6 +7,7 @@ public class Combo : MonoBehaviour
     bool _isIdle = true;
     public Animator ani;
     int _nextCombo = 0;
+    public List<GameObject> meleeHitboxes = new List<GameObject>();
 
     void Update()
     {
@@ -56,5 +57,39 @@ public class Combo : MonoBehaviour
         _nextCombo = 0;
         ani.SetTrigger("Idle");
         _isIdle = true;
+    }
+
+    public void HitBoxMelee1Activate()
+    {
+        meleeHitboxes[0].SetActive(true);
+    }
+
+
+    public void HitBoxMelee2Activate()
+    {
+        meleeHitboxes[1].SetActive(true);
+
+    }
+
+    public void HitBoxMelee3Activate()
+    {
+        meleeHitboxes[2].SetActive(true);
+
+    }
+
+    public void HitBoxMelee1Deactivate()
+    {
+        meleeHitboxes[0].SetActive(false);
+
+    }
+    public void HitBoxMelee2Deactivate()
+    {
+        meleeHitboxes[1].SetActive(false);
+
+    }
+    public void HitBoxMelee3Deactivate()
+    {
+        meleeHitboxes[2].SetActive(false);
+
     }
 }
