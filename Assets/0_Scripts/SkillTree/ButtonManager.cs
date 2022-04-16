ba94a35b0b6e4bef4ad1d38219ebe656dc650eaf
thieflin <ignaciogarrido1234@hotmail.com>
2022-04-16 13:33:22 -0300

    kINDA WORKING

---
+++ Assets/0_Scripts/SkillTree/ButtonManager.cs
@@ -0,0 +1,25 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI;
+//using UnityEngine.EventSystems;
+
+//public class ButtonManager : MonoBehaviour
+//{
+//    public GameObject backButton;
+//    [SerializeField] GameObject _purchaseText;
+//    Button button;
+
+//    public void OnButtonSelected()
+//    {
+//        if (Input.GetButtonDown("Interact"))
+//        {
+//            button.
+//            _purchaseText.SetActive(true);
+//            //Cuando lo activo elimino todo tipo de seleccion que tenga
+//            EventSystem.current.SetSelectedGameObject(null);
+//            //Elijo el nuevo boton selecionado
+//            EventSystem.current.SetSelectedGameObject(backButton);
+//        }
+//    }
+//}
