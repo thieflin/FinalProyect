f249901bbc0dfc0c185e53956fd014df20bbccfb
thieflin <ignaciogarrido1234@hotmail.com>
2022-05-14 22:08:30 -0300

    Ladies and gentlemen, its an honour to notify you all that we, finally, do have VFX

---
+++ Assets/ActivateParticle.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateParticle : MonoBehaviour
+{
+    //[SerializeField] ParticleSystem particle;
+
+    //private void Start()
+    //{
+    //    particle = GetComponent<ParticleSystem>();
+        
+    //}
+
+    //private void OnEnable()
+    //{
+    //    ParticleSystem.EmissionModule em = particle.emission;
+    //    em.enabled = true;
+    //}
+}
