30a882e8097ffc35f4abe8a110776a24311de700
fran110996 <franmenzella@gmail.com>
2022-05-24 01:53:25 -0300

    reformed ranged y retoques

--- Assets/0_Scripts/Enemy/RangeEnemy/RangeEnemy.cs
+++ Assets/0_Scripts/Enemy/RangeEnemy/RangeEnemy.cs
@@ -8,20 +8,31 @@ public class RangeEnemy : Enemy
     public GameObject bulletPrefab;
     [SerializeField] private float _timeBtwShots;
     [SerializeField] private float startTimeBtwShots;
+    public Rigidbody _rb;
 
     [Header("Move range enemy")]
     public float stoppingDistance;
     public float goBackDistance;
     public int multiplierRunningAway;
+    public float backDashForce;
     private Ray checkWallsRay;
+    public float timeForDash;
     [SerializeField] LayerMask _wallMask;
+    bool onCooldown;
+    bool canAttack;
+    public float secondsOnStay;
 
-    [Header("Ranged Protector")]
-    public ProtectorEnemy protectorPrefab;
-    public float percentHPToInvoke;
-    public bool protectorInvoked;
-    public float offSetYProtector;
-    public float offSetForwardProtector;
+    //[Header("Ranged Protector")]
+    //public ProtectorEnemy protectorPrefab;
+    //public float percentHPToInvoke;
+    //public bool protectorInvoked;
+    //public float offSetYProtector;
+    //public float offSetForwardProtector;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
 
     void Update()
     {
@@ -32,6 +43,7 @@ public class RangeEnemy : Enemy
             Vector3 lookAt = (player.transform.position - transform.position).normalized;
             lookAt.y = 0f;
             transform.forward = lookAt;
+
             Attack();
 
             //PARA QUE SE VAYA PARA ATRAS SOLO CUANDO NO HAYA UNA PARED
@@ -41,12 +53,12 @@ public class RangeEnemy : Enemy
             }
         }
 
-        if ((this.gameObject.GetComponent<EnemyData>().GetHP() / this.gameObject.GetComponent<EnemyData>().GetMaxHP()) < percentHPToInvoke)
-        {
-            if (!protectorInvoked) { 
-                InvocarProtector();
-            }
-        }
+        //if ((this.gameObject.GetComponent<EnemyData>().GetHP() / this.gameObject.GetComponent<EnemyData>().GetMaxHP()) < percentHPToInvoke)
+        //{
+        //    if (!protectorInvoked) { 
+        //        InvocarProtector();
+        //    }
+        //}
 
         if (!playerIsInSight && !isFacingWall())
         {
@@ -65,48 +77,86 @@ public class RangeEnemy : Enemy
 
     private void Attack()
     {
-        if (_timeBtwShots <= 0)
+        if (CanAttack())
         {
-            Instantiate(bulletPrefab, transform.position, Quaternion.identity);
-            _timeBtwShots = startTimeBtwShots;
+            if (_timeBtwShots <= 0)
+            {
+                Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+                _timeBtwShots = startTimeBtwShots;
+            }
+            else
+            {
+                _timeBtwShots -= Time.deltaTime;
+            }
         }
+    }
+
+    bool CanAttack()
+    {
+        Vector3 playerPosition = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+
+        if (Vector3.Distance(transform.position, player.transform.position) < rangeAttack && playerIsInSight)
+            return true;
         else
-        {
-            _timeBtwShots -= Time.deltaTime;
-        }
+            return false;
     }
 
     private void Move()
     {
         Vector3 playerPosition = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
 
-        if (Vector3.Distance(transform.position, player.transform.position) > stoppingDistance && playerIsInSight)
+        if (Vector3.Distance(transform.position, player.transform.position) < goBackDistance && playerIsInSight)
+        {
+            StartCoroutine(DashBack());
+        }
+
+        if (Vector3.Distance(transform.position, player.transform.position) > stoppingDistance && playerIsInSight && !onCooldown) //ON SLEEPING
         {
             transform.position = Vector3.MoveTowards(transform.position, playerPosition, movementSpeed * Time.deltaTime);
         }
-        else if (Vector3.Distance(transform.position, player.transform.position) < stoppingDistance && Vector3.Distance(transform.position, player.transform.position) > goBackDistance)
-        {
-            transform.position = this.transform.position;
-        }
-        else if (Vector3.Distance(transform.position, player.transform.position) < goBackDistance)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, playerPosition, -movementSpeed * multiplierRunningAway * Time.deltaTime);
-        }
+
     }
 
-    private void InvocarProtector()
+    IEnumerator DashBack()
     {
-        Debug.Log("InvocarProtector");
-
-        Vector3 spawnPosition = new Vector3(transform.position.x, transform.position.y + offSetYProtector, transform.position.z) + transform.forward * offSetForwardProtector;
-
-        var createdProtector = Instantiate(protectorPrefab, spawnPosition, Quaternion.identity);
-        createdProtector.owner = this;
-        protectorInvoked = true;
+        if (!onCooldown)
+        {
+            _rb.AddForce(backDashForce * transform.forward * -1 * Time.deltaTime, ForceMode.Force);
+            yield return new WaitForSeconds(timeForDash);
+            _rb.velocity = Vector3.zero;
+            onCooldown = true;
+            yield return new WaitForSeconds(secondsOnStay);
+            onCooldown = false;
+        }
     }
+
+    //private void InvocarProtector()
+    //{
+    //    Debug.Log("InvocarProtector");
+
+    //    Vector3 spawnPosition = new Vector3(transform.position.x, transform.position.y + offSetYProtector, transform.position.z) + transform.forward * offSetForwardProtector;
+
+    //    var createdProtector = Instantiate(protectorPrefab, spawnPosition, Quaternion.identity);
+    //    createdProtector.owner = this;
+    //    protectorInvoked = true;
+    //}
 
     private void OnDrawGizmos()
     {
         Debug.DrawRay(transform.position, transform.forward * -8, Color.red);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(WaitSecondTakeOffForce());
+        }
+    }
+
+    IEnumerator WaitSecondTakeOffForce()
+    {
+        yield return new WaitForSeconds(0.05f);
+        _rb.velocity = Vector3.zero;
     }
 }
