db24ccbef0877b57dcd3f72fdec65e2a8ad59bdc
fran110996 <franmenzella@gmail.com>
2022-06-06 21:45:57 -0300

    Palancas

--- Assets/ExtraScriptTest.cs
+++ Assets/ExtraScriptTest.cs
@@ -6,7 +6,10 @@ public class ExtraScriptTest : MonoBehaviour
 {
     public GameObject puenteActivate;
     public GameObject colliderToDestroy;
+    public List<GameObject> checkPointRamp = new List<GameObject>();
+    public List<GameObject> collidersToSwitch = new List<GameObject>();
     public int normalAttackLayer;
+
 
     private void OnTriggerEnter(Collider other)
     {
@@ -16,6 +19,25 @@ public class ExtraScriptTest : MonoBehaviour
             //AudioManager.PlaySound("puzzle");
             puenteActivate.GetComponent<Animator>().SetTrigger("Work");
             GetComponent<Animator>().SetTrigger("LeverHit");
+
+            if(checkPointRamp.Count > 0)
+            {
+                foreach (var checkPoint in checkPointRamp)
+                {
+                    if (!checkPoint.gameObject.activeSelf)
+                        checkPoint.SetActive(true);
+                    else
+                        checkPoint.SetActive(false);
+                }
+          
+            }
+            if (collidersToSwitch.Count > 0)
+            {
+                foreach (var collider in collidersToSwitch)
+                {
+                    collider.GetComponent<BoxCollider>().enabled = !collider.GetComponent<BoxCollider>().enabled;
+                }
+            }
 
             Destroy(colliderToDestroy);
         }
