2b319dcf966cda18545aa19f2424174053fab3d2
thieflin <ignaciogarrido1234@hotmail.com>
2022-06-25 20:04:44 -0300

    CAMBIOS TRIPLE C PARA QUE NOS EBUGGEE EL DASH

--- Assets/0_Scripts/Combos/Combo.cs
+++ Assets/0_Scripts/Combos/Combo.cs
@@ -62,10 +62,10 @@ public class Combo : MonoBehaviour
     }
     void Update()
     {
-
+        Debug.Log(canAttack);
         //Dash activation negated
         if (ani.GetCurrentAnimatorStateInfo(0).IsName("AttackOne") || ani.GetCurrentAnimatorStateInfo(0).IsName("AttackTwo")
-                                || ani.GetCurrentAnimatorStateInfo(0).IsName("AttackThree"))
+                                || ani.GetCurrentAnimatorStateInfo(0).IsName("AttackThree") || ani.GetCurrentAnimatorStateInfo(0).IsName("Gun1"))
         {
             _pm.flagDash = false;
         }
