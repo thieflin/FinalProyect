f620796e4bab7eb60b22a1e4c4b4cef3a2f43f4a
thieflin <ignaciogarrido1234@hotmail.com>
2022-06-28 07:09:52 -0300

    Balance enemigos en rango, dmg y vida, juste colliders arriba a la ziquierda

--- Assets/0_Scripts/UI/MenuButtonSelector.cs
+++ Assets/0_Scripts/UI/MenuButtonSelector.cs
@@ -10,7 +10,7 @@ public class MenuButtonSelector : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        EventSystem.current.SetSelectedGameObject(startButton);
     }
 
     // Update is called once per frame
