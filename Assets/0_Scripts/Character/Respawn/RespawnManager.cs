f7f660c7bbc5ee08bb56178511e3651e083d3971
fran110996 <franmenzella@gmail.com>
2022-06-28 03:23:07 -0300

    Medkit

--- Assets/0_Scripts/Character/Respawn/RespawnManager.cs
+++ Assets/0_Scripts/Character/Respawn/RespawnManager.cs
@@ -14,6 +14,7 @@ public class RespawnManager : MonoBehaviour
 
     private void Start()
     {
+
     }
 
     public void RespawnCharacter()
--- Assets/MedKit.cs
+++ Assets/MedKit.cs
@@ -2,17 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MedKit : MonoBehaviour
+public class MedKit : Objects
 {
-    // Start is called before the first frame update
-    void Start()
+    public int healAmount;
+    public GameObject firstChild;
+    bool used;
+
+    private void Start()
     {
-        
+        if (transform.GetChild(0) != null)
+            firstChild = transform.GetChild(0).gameObject;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void TakeDamage(int dmg)
     {
-        
+        _maxHits -= dmg;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == _hitboxLayermask && !used)
+        {
+            TakeDamage(_hitCost);
+
+            if (_maxHits <= 0)
+            {
+                var charStatus = other.transform.parent.GetComponent<CharStatus>();
+                charStatus.hp += healAmount;
+                if (charStatus.hp >= charStatus.maxHp)
+                {
+                    charStatus.hp = charStatus.maxHp;
+                }
+
+                charStatus._hpBar.fillAmount = charStatus.HpPercentCalculation(charStatus.hp);
+
+                used = true;
+
+                firstChild.SetActive(false);
+            }
+        }
     }
 }
