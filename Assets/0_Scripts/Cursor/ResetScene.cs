ae7bdfdbbd2f1ae0f1fca575a5c4ae47371bbf07
fran110996 <franmenzella@gmail.com>
2022-08-26 01:45:41 -0300

    Resetscene

---
+++ Assets/0_Scripts/Cursor/ResetScene.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetScene : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
